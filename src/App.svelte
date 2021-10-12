447a128fd9e28123efe92d751ff7e7a04d413305
Joshua J. Haller <phone@joshuajhaller.com>
2021-10-12 15:17:41 -0700

    Installed node packages for bootstrap, node-sass, postcss and svelte-preprocess. Used some scss in the App to change colors and imported Bootstrap for styling.

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,18 @@
 </script>
 
 
-<main>
-
+<div class="container">
   <h1>Hello {name}!</h1>
-</main>
+  <p class="text-primary">
+    Bacon ipsum dolor amet frankfurter tri-tip ham, doner ground round biltong salami spare ribs ball tip strip steak filet mignon meatloaf pork chop buffalo corned beef. Cow beef shank doner picanha tail. Ham short ribs shankle, salami shoulder ground round filet mignon chuck swine strip steak prosciutto t-bone bresaola. Swine ham turducken tri-tip chislic ham hock picanha rump t-bone capicola ground round short ribs filet mignon.
+  </p>
+</div>
 
-<style>
+<style lang="scss">
+  $primary: purple;
+
+  @import "../node_modules/bootstrap/scss/bootstrap.scss";
     h1 {
-      color: purple;
+      color: $primary;
     }
 </style>
